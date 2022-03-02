a73d155c290a690a469a3cbc47c39495bc7a59a3
perplex0204 <83807755+perplex0204@users.noreply.github.com>
2022-03-02 13:30:01 +0800

    Update code.py

--- NCCU - 計算直線方程/code.py
+++ NCCU - 計算直線方程/code.py
@@ -12,9 +12,6 @@ def find_function(point_a, point_b):
     else:
         c = 1
 
-    point_a = (130, 15)
-    point_b = (6, 20)
-
     matrix1 = numpy.array([[point_a[0], 1], [point_b[0], 1]])
     matrix2 = numpy.array([point_a[1], point_b[1]])
     sol = numpy.linalg.solve(matrix1, matrix2)
@@ -27,4 +24,12 @@ def find_function(point_a, point_b):
     return answer
 
 
-print(find_function((1, 2), (1, 5)))
+while True:
+    try:
+        x1, y1 = input('請輸入第一個數組(兩個數字)，並用空白隔開：').split(' ')
+        point_a = (float(x1), float(y1))
+        x2, y2 = input('請輸入第二個數組(兩個數字)，並用空白隔開：').split(' ')
+        point_b = (float(x2), float(y2))
+        print(find_function(point_a, point_b))
+    except ValueError:
+        print('請重新輸入正確數值')
